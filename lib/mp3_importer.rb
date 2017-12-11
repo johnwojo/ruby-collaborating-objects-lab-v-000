da271d55a25215446ddf1785f814012817587ee1
John Wojciechowski <jwojo13@gmail.com>
2017-12-11 20:22:15 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -15,4 +15,8 @@ attr_accessor :path
   def import
     self.files.each do |filename|
       song_data = filename.split(" - ")
+      artist = Artist.find_or_create_by_name(song_data[0])
+      artist.save
+      song = Song.new(song_data[1])
+      artist.add_song(song)
 end
