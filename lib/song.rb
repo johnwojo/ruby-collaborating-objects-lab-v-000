0c81e7168ed335e15f0c5d72544ea7849cb81e95
John Wojciechowski <jwojo13@gmail.com>
2017-12-11 20:19:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,6 +17,7 @@ class Song
   def self.new_by_filename(filename)
     song = self.new(filename.split(" - ")[1])
     song.artist = Artist.find_or_create_by_name(filename.split(" - ")[0])
+    song
   end
 
 end
